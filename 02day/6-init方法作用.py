fe1fc164476e8117903d6be47617d7c28d47d907
xiaoyuanandroid <75216511@qq.com>
2018-04-27 15:13:34 +0800

    笔记

--- 02day/6-init方法作用.py
+++ 02day/6-init方法作用.py
@@ -1,10 +1,11 @@
+#self 谁调用就指向谁
 class Girl():
 	#用初始化对象的一些属性	
 	def __init__(self,age,height):
 		self.age = age #相等于louxueman.age = 12
 		self.height = height
 		self.address = "河南"
-		self.games = []	
+		self.games = []#定义空列表	
 
 	def study(self):
 		print("学习")
@@ -14,6 +15,9 @@ class Girl():
 
 	def showage(self):
 		print("年龄%d"%self.age)
+
+	def addgames(self,game):
+		self.games.append(game)
 
 	def showgames(self):
 		for i in self.games:
@@ -25,10 +29,15 @@ class Girl():
 louxueman = Girl(12,170)
 louxueman.study()
 louxueman.opencar("宝马")
-louxueman.showage()i
+louxueman.showage()
 #加游戏
+louxueman.addgames("王者")
 louxueman.showgames()
+print(id(louxueman))
 
 xiaoyuan  = Girl(16,180)
 xiaoyuan.study()
 xiaoyuan.opencar("拖拉机")
+xiaoyuan.addgames("最强谈谈")
+xiaoyuan.showgames()
+print(id(xiaoyuan))
