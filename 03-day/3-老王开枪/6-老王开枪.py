1af12b6d7ef8e15c414e9ceb91bbf19153a1f26c
xiaoyuanandroid <75216511@qq.com>
2018-05-02 14:53:08 +0800

    老王开枪

---
+++ 03-day/3-老王开枪/6-老王开枪.py
@@ -0,0 +1,61 @@
+#人类
+class Person():
+	def __init__(self,name):
+		self.name = name
+		self.gun = None
+
+	def zhuangzidan(self,danjia,bullet):
+		danjia.addBullet(bullet)
+
+	def zhuangdanjia(self,gun,danjia):
+		gun.addDanJia(danjia)
+
+	def takeGun(self,gun):#老王拿枪
+		self.gun = gun
+
+	def openGun(self,diren):
+		#拿到一发子弹
+		zidan = self.gun.popGunBullet()
+
+#枪类
+class Gun():
+	def __init__(self,name):
+		self.name = name
+		self.danjia = None
+	#装弹夹
+	def addDanJia(self,danjia):
+		self.danjia = danjia
+		print(id(self.danjia))
+
+	def popGunBullet(self):
+		return self.danjia.popBullet()	
+#弹夹
+class DanJia():
+	def __init__(self,size):
+		self.size = size
+		self.bullet_list = []
+	
+	def addBullet(self,bullet):
+		self.bullet_list.append(bullet)
+		print(len(self.bullet_list))
+
+	def popBullet(self):
+		return self.bullet_list.pop()		
+#子弹
+class Bullet():
+	pass
+
+
+
+laowang = Person("老王")
+ak47 = Gun("ak47")
+danjia = DanJia(20)#可以放20颗子弹
+for i in range(20):
+	bullet = Bullet()
+	laowang.zhuangzidan(danjia,bullet)#装子弹
+laowang.zhuangdanjia(ak47,danjia)#装弹夹
+
+laosong = Person("老宋")#创建一个人
+laowang.takeGun(ak47)#老王拿枪
+laowang.openGun(laosong)
+
